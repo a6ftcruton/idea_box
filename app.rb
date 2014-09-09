bbfaf88f3295b5b2c1dbb37fe99c8e4eb9b335ba
Jim Sutton <jimsuttonjimsutton@gmail.com>
2014-09-09 15:49:31 -0600

    First Commit

---
+++ app.rb
@@ -0,0 +1,34 @@
+require './idea'
+
+class IdeaBoxApp < Sinatra::Base
+  set :method_override, true
+
+  get '/' do
+    erb :index, locals: {ideas: Idea.all}
+  end
+
+  post '/' do
+    idea = Idea.new(params[:idea])
+    idea.save
+    redirect '/'
+  end
+
+  delete '/:id' do |id|
+    Idea.delete(id.to_i)
+    redirect '/'
+  end
+
+  get '/:id/edit' do |id|
+    idea = Idea.find(id.to_i)
+    erb :edit, locals: {idea_id: id, idea: idea}
+  end
+
+  put '/:id' do |id|
+    Idea.update(id.to_i, params[:idea])
+    redirect '/'
+  end
+
+  not_found do
+    erb :error
+  end
+end
---
+++ config.ru
@@ -0,0 +1,7 @@
+# in charge of startup and requires for the app
+require 'bundler'
+Bundler.require
+
+require './app.rb'
+
+run IdeaBoxApp
